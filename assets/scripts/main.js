05b5c2019815caa7e089dbd4a66ab4f18e5d8b6e
Ha Na Gill <oneway.first@gmail.com>
2015-12-01 16:39:24 -0500

    projects tab toggle jQuery

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,9 @@
-$('.carousel').carousel({
-  interval: 5000
-})
+$('#webdev-tab').click(function(){
+	$('.web-projects-box').show();
+	$('.design-projects-box').hide();
+});
+
+$('#design-tab').click(function(){
+	$('.web-projects-box').hide();
+	$('.design-projects-box').show();
+});
