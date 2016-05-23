8bd37ef2f8deb83f8bd4c232ada1c26ae4908a79
oneway101 <oneway.first@gmail.com>
2016-05-23 18:48:12 -0400

    starting the Starfield

--- assets/scripts/starfield.js
+++ assets/scripts/starfield.js
@@ -26,9 +26,71 @@ Starfield.prototype.initialize = function(div){
 
   //create canvas
   var canvas = document.createElement('canvas');
+  console.log(canvas)
   div.appendChild(canvas);
   this.canvas = canvas;
   this.canvas.width = this.width;
   this.canvas.height = this.height;
 
 };
+
+Starfield.prototype.start = function(){
+  //create stars
+  var stars = [];
+  for (var i = 0; i < this.stars; i++){
+    stars[i] = new Star(Math.random()*this.width, Math.random()*this.height,
+      Math.random()*3+1, (Math.random()*(this.maxVelocity - this.minVelocity)+this.minVelocity));
+  }
+  this.stars = stars;
+  console.log(stars);
+  //start the timer
+  this.intervalId = setInterval(function(){
+    self.update();
+    self.draw();
+  },1000/this.fps);
+};
+
+Starfield.prototype.stop = function() {
+  clearInterval(this.intervalId);
+};
+
+Starfield.prototype.update = function() {
+  var dt = 1 / this.fps;
+  for(var i=0; i<this.stars.length; i++) {
+      var star = this.stars[i];
+      star.y += dt * star.velocity;
+      //  If the star has moved from the bottom of the screen, spawn it at the top.
+      if(star.y > this.height) {
+          this.stars[i] = new Star(Math.random()*this.width, 0, Math.random()*3+1, 
+             (Math.random()*(this.maxVelocity - this.minVelocity))+this.minVelocity);
+      }
+  }
+};
+Starfield.prototype.draw = function() {
+    
+    //  Get the drawing context.
+    var ctx = this.canvas.getContext("2d");
+ 
+    // Draw the background.
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(0, 0, this.width, this.height);
+ 
+    //  Draw stars.
+    ctx.fillStyle = '#ffffff';
+    for(var i=0; i<this.stars.length;i++) {
+        var star = this.stars[i];
+        ctx.fillRect(star.x, star.y, star.size, star.size);
+    }
+};  
+
+function Star(x,y,size,velocity){
+  this.x = x;
+  this.y = y;
+  this.size = size;
+  this.velocity = velocity;
+}
+
+var starfieldDiv = document.getElementById('starfield');
+var starfield = new Starfield();
+starfield.initialize(starfieldDiv);
+starfield.start();
