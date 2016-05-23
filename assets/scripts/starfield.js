399f101c4e6d6094a593f1aa1c0dbb1ec87b2e49
oneway101 <oneway.first@gmail.com>
2016-05-23 16:38:04 -0400

    initialize the Starfield

--- assets/scripts/starfield.js
+++ assets/scripts/starfield.js
@@ -8,3 +8,27 @@ function Starfield() {
   this.stars = 100;
   this.intervalId = 0;
 }
+
+Starfield.prototype.initialize = function(div){
+  var self = this;
+
+  this.starfieldDiv = div
+  self.width = window.innerWidth;
+  self.height = window.innerHeight;
+
+  window.addEventListener('resize',function resize(event){
+    self.width = window.innerWidth;
+    self.height = window.innerHeight;
+    self.canvas.width = self.width;
+    self.canvas.height = self.height;
+    self.draw();
+  });
+
+  //create canvas
+  var canvas = document.createElement('canvas');
+  div.appendChild(canvas);
+  this.canvas = canvas;
+  this.canvas.width = this.width;
+  this.canvas.height = this.height;
+
+};
